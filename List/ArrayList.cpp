ed439e984e1782f0eb07a14cb81812ed3cfb3e6b
Jack King <king.jack.top@gmail.com>
2016-09-04 00:34:28 +0800

    ArrayList

---
+++ List/ArrayList.cpp
@@ -0,0 +1,104 @@
+// 线性表的顺序表示
+// Created by Jack King on 9/2/16.
+//
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+#define LIST_INIT_SIZE 100  //线性表存储空间的初始分配量
+#define LIST_INCREMENT 10    //线性表存储空间的分配增量
+
+template<typename ElemType>
+struct ArrayList {
+    ElemType *elem; // 存储空间基址, start with 0
+    int length; // 当前线性表的长度, start with 1
+    int list_size;  // 当前分配的存储容量(以sizeof(ElemType)为单位), start with 1
+
+    ArrayList() {
+        // 构造一个空的线性表
+        try {
+            this->elem = (ElemType *) malloc(LIST_INIT_SIZE * sizeof(ElemType));
+            this->length = 0; //空表长度为0
+            this->list_size = LIST_INIT_SIZE; //初始存储容量
+        } catch (std::exception e) {
+            // 分配空间异常,退出程序
+            std::cout << e.what() << std::endl;
+            exit(1);
+        }
+    }
+
+    ~ArrayList() {
+        // 释放线性表
+        free(this->elem);
+    }
+
+    void expand(size_t size = LIST_INCREMENT) {
+        try {
+            realloc(this->elem, this->list_size + size);   // 对空间长度扩容
+            this->list_size += size;  // update List length
+        } catch (std::exception e) {
+            // 分配空间异常,退出程序
+            std::cout << e.what() << std::endl;
+            exit(1);
+        }
+
+    }
+
+
+    void insert(ElemType ele, int pos) {
+        // pos start with 0, insert in front of pos
+        if (pos < 0 || pos > this->length) {
+            std::cout << "Error: pos out of list at insert" << std::endl;
+            exit(1);
+        } else {
+            if (this->length == this->list_size)
+                this->expand(); // expand default size if list_size limited
+            for (int i = this->length - 1; i >= pos; i--) {
+                this->elem[i + 1] = this->elem[i];
+            }
+            this->elem[pos] = ele;
+            this->length += 1;
+        }
+
+    }
+
+    void erase(int pos) {
+        // erase element at pos. pos start with 0
+        if (pos < 0 || pos > this->length) {
+            std::cout << "Error:pos out of list at erase" << std::endl;
+            exit(1);
+        } else {
+            for (int i = pos; i < this->length - 2; i++) {
+                this->elem[i] = this->elem[i + 1];
+            }
+            this->length -= 1;
+        }
+    }
+
+    void push_back(ElemType ele) {
+        this->insert(ele, this->length);
+    }
+
+    ElemType pop_back() {
+        this->erase(this->length - 1);
+        return this->elem[this->length];
+    }
+};
+
+// test
+using namespace std;
+
+int main() {
+    ArrayList<int> sq_list;
+    for (int i = 1; i < 10; i++) {
+        sq_list.push_back(i);
+    }
+    for (int i = 1; i < 10; i++) {
+        cout << sq_list.pop_back() << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
