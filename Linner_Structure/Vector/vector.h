8e2881377c0bedfd38fb43c0cfc86fe34813b23f
Jack <email@qiaohong.org>
2017-07-17 17:08:09 +0800

    moved some file, update vector api

--- Linner_Structure/Vector/vector.h
+++ Linner_Structure/Vector/vector.h
@@ -1,20 +1,49 @@
-//
-// Created by Jack King on 5/28/17.
-//
+/*
+*
+*/
+
+#ifndef VECTOR_H
+#define VECTOR_H
 
 // vector's maxsize will multiple EXTEND_FACTOR if vector's size extended to maxsize
 #define EXTEND_FACTOR 2
 
-
-// vector struct
 typedef struct {
-    void *data;         // data pointer
+    void *data;             // data pointer
     unsigned int size;      // current used size
-    unsigned int maxsize;   // malloced vector size
-} vector;
+    unsigned int maxsize;   // allocated vector size
+} Vector;
 
-// init function, call to return an vector
-vector new_vector(void *ElemType,unsigned int init_size);
+/*
+ * function: alloc a Vector struct and init it
+ * arguments:
+ *      init_size: vector's init size
+ *      data_size: size of data type
+ * efficiency: O(1)
+ * return value: success return a ptr, else NULL on error occurred
+ */
+Vector *Vector_malloc(unsigned int init_size, unsigned int data_size);
 
-// destructor
-void delete_vector(vector tmp);
+/*
+ * function: free vector and data memory
+ * arguments:
+ *      vec: vector pointer
+ * efficiency: O(1)
+ * return value: success return 0, else -1 on error occurred
+ */
+int Vector_free(Vector *vec);
+
+/*
+ * function: resize a vector. if new size less than origin, those data will be lost.
+ * arguments:
+ *      vec: vector that need resize
+ *      size: new size of vector
+ *      data_size: size of data type
+ * efficiency: O(1)
+ * return value: success return 0, else -1 on error occurred
+ */
+int Vector_resize(Vector *vec, unsigned int size, unsigned int data_size);
+
+int Vector_push_back();
+
+#endif
