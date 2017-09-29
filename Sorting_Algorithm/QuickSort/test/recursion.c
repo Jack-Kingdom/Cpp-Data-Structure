dbbdf8546eaf71184638bc2a5cda595f35685742
jack <email@qiaohong.org>
2017-09-29 18:23:42 +0800

    add test with random number

--- Sorting_Algorithm/QuickSort/test/recursion.c
+++ Sorting_Algorithm/QuickSort/test/recursion.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "../header/quick_sort.h"
 
-#define ARRAY_LENGTH 100000
+#define ARRAY_LENGTH 1000000
 
 typedef struct {
     int number;
@@ -19,15 +19,10 @@ int main() {
     for (int i = 0; i < ARRAY_LENGTH; i++) array[i] = malloc(sizeof(DataType));
 
     srand(time(NULL));
-    for (int i = 0; i < ARRAY_LENGTH; i++) ((DataType*)array[i])->number
-        DataType *tmp = array[i];
-        tmp->number = i;
+    for (int i = 0; i < ARRAY_LENGTH; i++) ((DataType *) array[i])->number = rand();
 
     quick_sort(array, ARRAY_LENGTH, &cmp);
 
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-        DataType *tmp = array[i];
-        printf("%d ", tmp->number);
-    }
+    for (int i = 0; i < ARRAY_LENGTH; i++) printf("%d ", ((DataType *) array[i])->number);
 
 }
